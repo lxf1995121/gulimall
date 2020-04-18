5ec5f4c7862dce0b7815c6f5f2f552aba0c7d82c
xf_glb <liuxiaofeng@bluemoon.com.cn>
2020-04-18 18:52:00 +0800

    练习

--- gulimall-product/src/main/java/com/xf/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/xf/gulimall/product/GulimallProductApplication.java
@@ -1,8 +1,9 @@
 package com.xf.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.xf.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
--- gulimall-product/src/test/java/com/xf/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/xf/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,27 @@
 package com.xf.gulimall.product;
 
+import com.xf.gulimall.product.entity.BrandEntity;
+import com.xf.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class GulimallProductApplicationTests {
+    @Autowired
+    BrandService brandService;
 
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setBrandId(1L);
+        brandEntity.setName("小米");
+
+        brandService.updateById(brandEntity);
+        System.out.println("保存成功...");
     }
 
 }
